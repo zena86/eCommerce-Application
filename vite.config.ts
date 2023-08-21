ef669e9f6571713352c141e6bc16cb3bdccbcc50
Nikita Khrustalev <65180406+SadMearise@users.noreply.github.com>
2023-08-22 00:49:02 +0300

    fix: fix vite errors (#47)

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      stream: "rollup-plugin-node-polyfills/polyfills/stream",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
       },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
+      protocolImports: true,
+    }),
+  ],
+  define: {
+    global: "window",
   },
 });
