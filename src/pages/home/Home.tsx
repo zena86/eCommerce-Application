61a5bd3b2a1fc8e46f1dfd78e1db067e96c29ed3
Nikita Khrustalev <65180406+SadMearise@users.noreply.github.com>
2023-09-19 00:25:52 +0300

    fix: fix links and styles (#76)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { Button, Card, CardActions, CardContent, CardMedia, Container, Typography } from "@mui/material";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import AlertView from "../../components/alertView/AlertView";
 import Header from "../../components/header/Header";
 import isSuccess from "../../store/features/registration/registrationSelector";
@@ -9,6 +10,20 @@ import getProductCountFromCart from "../../utils/getProductCountFromCart";
 import { setCount } from "../../store/features/cartCount/cartCountSlice";
 import { useAppDispatch } from "../../store/hooks";
 import styles from "./Home.module.scss";
+import RouterPaths from "../../router/routes";
+
+const cards = [
+  {
+    name: "SAVE50",
+    description: "Enter the promotional code in the cart to receive a 50% discount",
+    image: "gift-svgrepo-com.svg",
+  },
+  {
+    name: "SAVE30",
+    description: "Enter the promotional code in the cart to receive a 30% discount",
+    image: "coupon-svgrepo-com.svg",
+  },
+];
 
 export default function Home() {
   const dispatch = useAppDispatch();
@@ -25,70 +40,49 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Container maxWidth="lg">
+      <Container
+        maxWidth="lg"
+        className={styles.container}
+      >
         <div className={styles["promo-code-wrapper"]}>
-          <h1>Available promo codes:</h1>
+          <h1 className={styles.title}>Available promo codes:</h1>
           <div className={styles["card-wrapper"]}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140, backgroundSize: "100% 100%" }}
-                image="gift-svgrepo-com.svg"
-                title="green iguana"
-              />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                >
-                  SAVE50
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  Enter the promotional code in the cart to receive a 50% discount
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  fullWidth
-                >
-                  go to Shopping Cart
-                </Button>
-              </CardActions>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140, backgroundSize: "100% 100%" }}
-                image="coupon-svgrepo-com.svg"
-                title="green iguana"
-              />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                >
-                  SAVE30
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  Enter the promotional code in the cart to receive a 30% discount
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  fullWidth
-                >
-                  go to Shopping Cart
-                </Button>
-              </CardActions>
-            </Card>
+            {cards.map((card) => (
+              <Card
+                sx={{ maxWidth: 345 }}
+                key={card.name}
+              >
+                <CardMedia
+                  sx={{ height: 140, backgroundSize: "100% 100%" }}
+                  image={card.image}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                  >
+                    {card.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {card.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    fullWidth
+                    component={Link}
+                    to={RouterPaths.Basket}
+                  >
+                    go to Shopping Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
           </div>
         </div>
       </Container>
